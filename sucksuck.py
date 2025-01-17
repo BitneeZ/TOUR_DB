9ef603f5a84869f4c0be593e87779906436855a4
IP3RFECTI <144429392+IP3RFECTI@users.noreply.github.com>
2025-01-17 21:26:58 +0300

    1.5 parsers ready

--- sucksuck.py
+++ sucksuck.py
@@ -1,1 +1,13 @@
-# Testing pull
+import pandas as pd
+
+# Загрузка файла
+file_path = 'moscow_attractions_updated.csv'
+data = pd.read_csv(file_path)
+
+# Замена запятой на точку в рейтинге
+if 'Рейтинг' in data.columns:
+    data['Рейтинг'] = data['Рейтинг'].astype(str).str.replace(',', '.')
+
+# Сохранение обновленного файла
+updated_file_path = 'moscow_attractions_rating_corrected.csv'
+data.to_csv(updated_file_path, index=False)
