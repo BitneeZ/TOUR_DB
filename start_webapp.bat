9f292fd26dac80a10d7cb33485fc676f00252cd2
BitneeZ <pismak.jora@gmail.com>
2025-01-21 00:34:12 +0300

    Переделал bat для запуска сайта

--- start_webapp.bat
+++ start_webapp.bat
@@ -3,13 +3,13 @@ REM Перейти в директорию, где находится bat-фай
 cd /d "%~dp0"
 
 REM Запуск локального сервера Python в папке project
-start "" python -m http.server --directory "project"
+start "" python -m http.server 8032 --directory "project"
 
 REM Задержка для запуска сервера
 timeout /t 2 > nul
 
-REM Открытие index.html в браузере по адресу http://localhost:8000/
-start "" "http://localhost:8000/"
+REM Открытие index.html в браузере по адресу http://localhost:8032/
+start "" "http://localhost:8032/"
 
 REM Завершение работы
 exit
